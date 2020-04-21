1de2cbdc9a47df1f127633f781c7cd37d040dc4a
Flagy <supericky94@msn.com>
2020-04-21 14:29:43 +0200

    Redirecting empty POST to index

--- server.py
+++ server.py
@@ -14,35 +14,36 @@ filename_global=""
 
 @app.route('/', methods=['GET', 'POST'])
 def main_page():
-    if request.method == 'POST':
-        file = request.files['file']
-        filename = secure_filename(file.filename)
-        file.save(os.path.join('uploads', filename))
-        return redirect(url_for('prediction', filename=filename))
+    print(request)
+    try:
+        if request.method == 'POST':
+            file = request.files['file']
+            filename = secure_filename(file.filename)
+            file.save(os.path.join('uploads', filename))
+            return redirect(url_for('prediction', filename=filename))
+    except:
+        pass
     return render_template('index.html')
 
 @app.route('/prediction/<filename>')
 def prediction(filename):
     global filename_global
     filename_global=filename
-    #Step 1
+    # Step 1
     my_image =cv2.imread(os.path.join("uploads",filename))
-    #Step 2
+    # Step 2
     my_image_re = cv2.resize(my_image, (299,299))
     my_image_re=(my_image_re.astype('float16'))/255.0
-    
+    # Prediction
     prediction=test.predict(my_image_re)
     print(prediction)
+    # Order the lists by decreasing probability
     zipped_lists = list(zip(prediction[0], prediction[1][0]))
     sorted_pairs = sorted(zipped_lists, key = lambda x: x[1])
     tuples = zip(*sorted_pairs)
     list1, list2 = [ list(tuple) for tuple in tuples]
     print(list1)
     print(list2)
-            
-        
-    
-    
     predictions = {
         "class1":list1[2],
         "class2":list1[1],
@@ -51,8 +52,8 @@ def prediction(filename):
         "prob2":list2[1],
         "prob3":list2[0],
       }
-#Step 5
     return render_template('prediction.html', predictions=predictions)
+
 @app.route("/partialFit/", methods=['GET','POST'])
 def move_forward():
     print(filename_global)
@@ -65,4 +66,4 @@ def move_forward():
     return render_template('ty.html')
 
 
-app.run(host='0.0.0.0', port=80)
+app.run(debug=False)
--- testing.py
+++ testing.py
@@ -8,21 +8,15 @@ Created on Wed Oct  3 11:24:38 2018
 
 import json
 from numpy import array
-
 import numpy as np
 import tensorflow as tf
 import pickle
 import os
 
-
-
-
-
 class Testing():
     
     def __init__(self):
-
-########################################LOAD MODEL################################  
+        # Load the model
         self.new_model = tf.keras.models.load_model('best/')
         self.new_model.summary()
         x_test = pickle.load(open("x_test.pkl", "rb"))
@@ -32,13 +26,12 @@ class Testing():
 
 
     def predict(self,img):
-        
         label=["Normale","Pneumonia","COVID"]
         img = np.expand_dims(img, axis=0)
         y=self.new_model.predict(img)
-       
-
         return ([label, y])
+    
+    
     def partial_fit(self,img,label):
         #self.new_model.partial_fit(img,label)
         pass
